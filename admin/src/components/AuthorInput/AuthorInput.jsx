6175e84e726c8296c85782598e4b18567947a56b
Pau <141435859+pauleenann@users.noreply.github.com>
2024-09-11 14:49:57 +0800

    add author modal

--- admin/src/components/AuthorInput/AuthorInput.jsx
+++ admin/src/components/AuthorInput/AuthorInput.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AuthorInput.css'
+import AuthorModal from '../AuthorModal/AuthorModal'
 
 const AuthorInput = () => {
+    const [open, setOpen] = useState(false)
   return (
     <div className="author-box">
         {/* authors */}
@@ -9,17 +11,19 @@ const AuthorInput = () => {
             {/* author name */}
                 <div className="author">
                     <span>Stan Lee</span>
-                        <button>
+                    <button>
                         <i class="fa-solid fa-xmark"></i>
                     </button>
                 </div>
         </div>
-            {/* button */}
-            <button className='add-new-author'>
-                <i class="fa-solid fa-plus"></i>
-                <span>Add new author</span>
-            </button>
-        </div>
+        {/* button */}
+        <button className='add-new-author' onClick={()=>setOpen(true)}>
+            <i class="fa-solid fa-plus"></i>
+            <span>Add new author</span>
+        </button>
+
+        <AuthorModal open={open} close={()=>setOpen(!open)}/>
+    </div>
   )
 }
 
