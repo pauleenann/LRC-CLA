e8da0f467b22ccc9fa35305628c8775fe7716595
Pau <141435859+pauleenann@users.noreply.github.com>
2024-10-14 18:59:29 +0800

    install libraries

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 import express from "express"
 import mysql from "mysql"
 import cors from "cors"
+import axios from 'axios'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.listen(8000,()=>{
+// api key for google books
+const apikey = "AIzaSyDq8MNGVWbLp-R-SFFe-SGL7Aa8CuMak0s";
+
+app.get('/bookData/:isbn',async (req,res)=>{
+    const isbn = req.params.isbn
+    try{
+        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${apikey}`);
+        console.log(response.data)
+        res.json(response.data);
+        
+    }catch(err){
+        console.log(err)
+    }
+})
+
+app.listen(3001,()=>{
     console.log('this is the backend')
 })
