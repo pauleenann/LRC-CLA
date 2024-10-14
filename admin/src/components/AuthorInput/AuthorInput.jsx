b8c107e646766a3c4bcae5bd7f1109fa7da0c82c
Pau <141435859+pauleenann@users.noreply.github.com>
2024-10-14 19:01:42 +0800

    display selected authors

--- admin/src/components/AuthorInput/AuthorInput.jsx
+++ admin/src/components/AuthorInput/AuthorInput.jsx
@@ -3,27 +3,32 @@ import './AuthorInput.css'
 import AuthorModal from '../AuthorModal/AuthorModal'
 import PublisherModal from '../PublisherModal/PublisherModal'
 
-const AuthorInput = () => {
+const AuthorInput = ({disabled,authors,handleChange,bookData, addAuthor}) => {
     const [open, setOpen] = useState(false)
+
   return (
     <div className="author-box">
         {/* authors */}
         <div className="authors">
             {/* author name */}
                 <div className="author">
-                    <span>Stan Lee</span>
+                    {bookData.authors?bookData.authors.map((item,key)=>{
+                        console.log(item)
+                        return <span>{item}</span>
+                    }):''}
+                    
                     <button>
                         <i class="fa-solid fa-xmark"></i>
                     </button>
                 </div>
         </div>
         {/* button */}
-        <button className='add-new-author' onClick={()=>setOpen(true)}>
+        <button className='add-new-author' onClick={()=>{disabled?setOpen(false):setOpen(true)}}>
             <i class="fa-solid fa-plus"></i>
             <span>Add new author</span>
         </button>
 
-        <AuthorModal open={open} close={()=>setOpen(!open)}/>
+        <AuthorModal open={open} close={()=>setOpen(!open)} handleChange={handleChange} bookData={bookData} addAuthor={addAuthor}/>
     </div>
   )
 }
