74a107d48cc14a1a5e58f70c8d6f501f1713f3bd
Gio <giolliana5sos@gmail.com>
2024-10-27 01:09:48 +0800

    add

--- client/src/components/Services/Services.jsx
+++ client/src/components/Services/Services.jsx
@@ -10,54 +10,46 @@ const Services = () => {
 
         {/* services */}
         <section className="container-fluid services">
-            <h1>Services</h1>
+            <h1>Departments</h1>
             <div className="row">
                 {/* service */}
                 <div className="card col-md-6">
                     <img className="card-img-top" src={service} alt="Card image cap"/>
                     <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 className="card-title">Entrepreneurship</h5>
+                    <p className="card-text">Learn about business planning, marketing, finance, and leadership to achieve your entrepreneurial goals.</p>
                     </div>
                 </div>
                 {/* service */}
                 <div className="card col-md-6">
                     <img className="card-img-top" src={service} alt="Card image cap"/>
                     <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 className="card-title">Languages</h5>
+                    <p className="card-text">Enhance your global perspective and communication skills.</p>
                     </div>
                 </div>
                 {/* service */}
                 <div className="card col-md-6">
                     <img className="card-img-top" src={service} alt="Card image cap"/>
                     <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 className="card-title">Hotel Management</h5>
+                    <p className="card-text">Learn about hotel operations, food and beverage management, event planning, and customer service to excel in the dynamic world of hospitality.</p>
                     </div>
                 </div>
                 {/* service */}
                 <div className="card col-md-6">
                     <img className="card-img-top" src={service} alt="Card image cap"/>
                     <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 className="card-title">Physical Education</h5>
+                    <p className="card-text">Learn about sportsmanship, teamwork, and the importance of regular exercise.</p>
                     </div>
                 </div>
                 {/* service */}
                 <div className="card col-md-6">
                     <img className="card-img-top" src={service} alt="Card image cap"/>
                     <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
-                </div>
-                {/* service */}
-                <div className="card col-md-6">
-                    <img className="card-img-top" src={service} alt="Card image cap"/>
-                    <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 className="card-title">Social Sciences</h5>
+                    <p className="card-text">Explore the complexities of human society and behavior.</p>
                     </div>
                 </div>
                 
