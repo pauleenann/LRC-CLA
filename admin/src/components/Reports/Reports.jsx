11b89a78983d6c961d227063b9a8e7327a22a3a6
Pau <141435859+pauleenann@users.noreply.github.com>
2024-12-31 13:48:48 +0800

    fix reports generation page

--- admin/src/components/Reports/Reports.jsx
+++ admin/src/components/Reports/Reports.jsx
@@ -7,113 +7,35 @@ const Reports = () => {
   return (
     <div className='reports-container'>
       <h1>Reports</h1>
-
-      {/* reports column */}
-      <div className="reports-filter-box">
-        {/* first column */}
-        <div className="reports-statistics">
-          {/* statistics */}
-          <div className="reports-statistics-box">
-          <p className='reports-dropdown'>
-              Acquisitions
-            </p>
-            <button>Acquisitions</button>
-            <button>Patrons</button>
-            <button>Circulation</button>
-            <button>Cash Register</button>
-          </div>
-
-          {/* Top Lists */}
-          <div className="reports-top-lists-box">
-            <p className='reports-dropdown'>
-              Top Lists
-            </p>
-            <button>Most Checkouts</button>
-            <button>Most Circulated</button>
-          </div>
-
-           {/* Inactive */}
-           <div className="reports-inactive-box">
-           <p className='reports-dropdown'>
-              Inactive
-            </p>
-            <button>Items with no checkouts</button>
-          </div>
-        </div>
-
         {/* reports summary */}
         <div className="reports-summary">
-          <p className='reports-summary-header'>Patrons Reports</p>
-          <p className='reports-summary-description'>Summary of patron statistics</p>
-
-          {/* reports filter */}
-          <div className="reports-summary-filter">
-
-            {/* date filter*/}
-            <div className="reports-date">
-              <label htmlFor="">Date Filter</label>
-              <div>
-                <input type="date" className='reports-filter-date'/>
-                <p>to</p>
-                <input type="date" className='reports-filter-date'/>
-              </div>
-            </div>
-
-             {/* college*/}
-             <div className="reports-college">
-              <label htmlFor="">College</label>
-              <div>
-                <div className='college-filter'>
-                  {/* selected colleges */}
-                  <div className='selected-colleges'>
-                    <div className='selected-college'>College of Science   <button>x</button>
-                    </div>
-                  </div>
-
-                  {/* dropdown */}
-                  <select name="" id="" className='college-dropdown'>
-                    <option value=""></option>
-                    <option value="">College of Science</option>
-                    <option value="">College of Liberal Arts</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-
-            {/* program*/}
-            <div className="reports-program">
-              <label htmlFor="">Program</label>
-              <div>
-                <div className='program-filter'>
-                  {/* selected colleges */}
-                  <div className='selected-programs'>
-                    <div className='selected-program'>All<button>x</button>
-                    </div>
-                  </div>
-
-                  {/* dropdown */}
-                  <select name="" id="" className='program-dropdown'>
-                    <option value=""></option>
-                    <option value="">BSIT</option>
-                    <option value="">BSIS</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-
-            {/* generate report button */}
-            <button className='generate-report-button'>
-              <img src={report} alt="" />
-              Generate Report
-            </button>
-           
-
-
+          <h4>Generate Reports</h4>
+          {/* type of report */}
+          <div className="report-type">
+            <label htmlFor="">Type of Report</label>
+            <select name="" id="">
+              <option value="" disabled selected>Select a type</option>
+            </select>
           </div>
-
-
+          {/* date range */}
+          <div className="date-range">
+            {/* start date */}
+            <div className="start-date">
+              <label htmlFor="start-date">Start date</label>
+              <input type="date" name="" id="start-date" />
+            </div>
+            {/* end date */}
+            <div className="end-date">
+              <label htmlFor="end-date">End date</label>
+              <input type="date" name="" id="end-date" />
+            </div>
+          </div>
+          <div className="buttons">
+            <button className="btn clear-btn">Clear</button>
+            <div className="btn generate-report">Generate Report</div>
+          </div>
         </div>
-      </div> 
+     
     </div>
   )
 }
