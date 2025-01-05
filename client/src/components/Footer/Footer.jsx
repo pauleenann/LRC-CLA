d22660a06e19ead5f27a61d18bfbbf66961ae74d
Pau <141435859+pauleenann@users.noreply.github.com>
2025-01-05 09:20:11 +0800

    fix frontend

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,6 +5,11 @@ import claLogo  from '../../assets/OPAC/icons/cla-logo.png'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
+  //display page from the top
+  const handleNavigate = () => {
+    window.scrollTo(0, 0);
+  };
+  
   return (
     <div className='footer-container'>
       {/* footer logo and college name */}
@@ -25,7 +30,7 @@ const Footer = () => {
       </div>
       {/* copyright */}
       <div className="tc-cpy">
-        <Link className='tc'>Terms & Conditions</Link>
+        <Link to='/terms-conditions' onClick={handleNavigate} className='tc'>Terms & Conditions</Link>
        <p className='m-0 copyright'>Copyright©2024. All Rights Reserved TUP-CLA Learning Resource Center</p>
       </div>
         
