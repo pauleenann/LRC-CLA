d067cea86e19137f0cbcb90d27cdf7f0c27c4ad4
Pau <141435859+pauleenann@users.noreply.github.com>
2025-02-24 16:49:00 +0800

    gawing clickable yung boxes sa dashboard

--- admin/src/components/DashBox/DashBox.jsx
+++ admin/src/components/DashBox/DashBox.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom';
+import './DashBox.css'
 
 const DashBox = ({icon, title,total}) => {
+  const navigate = useNavigate();
+
+  const handleClick = ()=>{
+    switch(title){
+      case 'Total Visits': 
+        navigate('/logbook');
+        break;
+      case 'Returned Resources':
+      case 'Overdue Resources':
+      case 'Borrowed Resources':
+        navigate('/circulation');
+        break;
+      default: 
+        console.log('Invalid. Please try again later')
+    }
+  }
   return (
-    <div className="dash-box col">
+    <div className="dash-box col" onClick={handleClick}>
         <div className="total-box">
             <span className='total'>{total}</span>
             <span className='label'>{title}</span>
