de3703b69f0e187d89b623b58d194951b34b665c
lancewrt <91044173+lancewrt@users.noreply.github.com>
2025-03-04 11:42:47 +0800

    initial new implementation of offline function
    the results of queries are passed on a variable in localstorage. the data from local storage will be used when there is no internet

--- admin/src/components/Logbook/Logbook try.jsx
+++ admin/src/components/Logbook/Logbook try.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import './Logbook.css'
-import search from '../../assets/Management System/logbook/search.svg'
-import exportIcon from '../../assets/Management System/logbook/export.svg'
-import left from '../../assets/Management System/logbook/arrow-left-red.svg'
-import right from '../../assets/Management System/logbook/arrow-right-red.svg'
+import { useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faArrowLeft, faArrowRight} from '@fortawesome/free-solid-svg-icons';
 import * as XLSX from 'xlsx'; // Import xlsx for Excel export
 
 const Logbook = () => {
@@ -15,12 +14,32 @@ const Logbook = () => {
     const [entriesPerPage, setEntriesPerPage] = useState(5);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalEntries, setTotalEntries] = useState(0); // Total number of entries
+    const [loading, setLoading] = useState(false)
+    const location = useLocation();
+
+    /* useEffect(() => {
+        getPatron();
+    }, [currentPage, entriesPerPage]); */
 
     useEffect(() => {
-        getPatron();
-    }, [currentPage, entriesPerPage]);
+        const params = new URLSearchParams(location.search);
+        const filterToday = params.get('filter') === 'today';
+
+        if (filterToday) {
+            fetchTodayEntries();
+            const interval = setInterval(fetchTodayEntries, 1000); // Fetch new logs every 1 seconds
+
+            return () => clearInterval(interval);
+        } else {
+            getPatron();
+            const interval = setInterval(getPatron, 1000); // Fetch new logs every 1 seconds
+
+            return () => clearInterval(interval);
+        }
+    }, [location.search, currentPage, entriesPerPage]);
 
     const getPatron = async () => {
+        setLoading(true)
         try {
             const params = {
                 search: searchInput,
@@ -30,11 +49,28 @@ const Logbook = () => {
                 page: currentPage, // Include current page in the request
             };
             const query = new URLSearchParams(params).toString();
-            const response = await axios.get(`http://localhost:3001/patronSort?${query}`);
+            const response = await axios.get(`http://localhost:3001/api/patron/sort?${query}`);
             setPatron(response.data.results); // Expect results array in response
             setTotalEntries(response.data.total); // Set total entries for pagination
+            
         } catch (err) {
             console.log(err.message);
+        }finally{
+            setLoading(false)
+        }
+    };
+
+    const fetchTodayEntries = async () => {
+        setLoading(true);
+        try {
+            const today = new Date().toISOString().split('T')[0]; // Get today's date
+            const response = await axios.get(`http://localhost:3001/api/patron/sort?startDate=${today}&endDate=${today}&limit=${entriesPerPage}&page=${currentPage}`);
+            setPatron(response.data.results);
+            setTotalEntries(response.data.total);
+        } catch (err) {
+            console.log(err.message);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -94,6 +130,7 @@ const Logbook = () => {
         }
     };
 
+    console.log(patron)
     return (
         <div className='logbook-container'>
             <h1>Logbook</h1>
@@ -108,78 +145,59 @@ const Logbook = () => {
                         aria-label="Search"
                         value={searchInput}
                         onChange={(e) => setSearchInput(e.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                              getPatron();
+                            }
+                          }}
                     />
                     <button className="btn log-search-button" onClick={getPatron}>
-                        <img src={search} alt="" /> Search
+                        <FontAwesomeIcon icon={faSearch} className='icon'/> 
                     </button>
                 </div>
-                <button className='btn logbook-export-button' onClick={exportToExcel}>
-                    <img src={exportIcon} alt="" />
-                    Export
-                </button>
+                
             </div>
 
             {/* filters */}
             <div className="logbook-filters">
-                <div className="logbook-entries-page">
                     <label htmlFor="entries">Entries per page</label>
                     <select
                         className="form-select"
                         value={entriesPerPage}
-                        onChange={(e) => setEntriesPerPage(Number(e.target.value))}
+                        onChange={(e) => setEntriesPerPage(e.target.value === "All" ? "All" : Number(e.target.value))}
                     >
                         <option value={5}>5</option>
                         <option value={10}>10</option>
                         <option value={20}>20</option>
+                        <option value="All">All</option>
                     </select>
-                </div>
-
-                <div className="logbook-date-filter">
-                    <label>Date Filter</label>
-                    <input
-                        type="date"
-                        className="logbook-filter-date"
-                        value={startDate}
-                        onChange={(e) => setStartDate(e.target.value)}
-                    />
-                    <p>to</p>
-                    <input
-                        type="date"
-                        className="logbook-filter-date"
-                        value={endDate}
-                        onChange={(e) => setEndDate(e.target.value)}
-                    />
-                    <button className="btn logbook-go-button" onClick={getPatron}>
-                        Go
-                    </button>
-                    <button className="btn logbook-clear-button" onClick={handleClear}>
-                        Clear
-                    </button>
-                </div>
+                
             </div>
 
             {/* table */}
-            <div className='logbook-table-box'>
+      
                 <table className='logbook-table'>
                     <thead>
                         <tr>
-                            <th>Number</th>
-                            <th>TUP ID</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Gender</th>
-                            <th>Phone No.</th>
-                            <th>Course</th>
-                            <th>College</th>
-                            <th>Date</th>
-                            <th>Time in</th>
+                            <td>No.</td>
+                            <td>TUP ID</td>
+                            <td>First Name</td>
+                            <td>Last Name</td>
+                            <td>Gender</td>
+                            <td>Phone No.</td>
+                            <td>Course</td>
+                            <td>College</td>
+                            <td>Date</td>
+                            <td>Time in</td>
                         </tr>
                     </thead>
                     <tbody>
-                        {patron.length > 0 ? (
+                        {Array.isArray(patron)?patron.length > 0 ? (
                             patron.map((item, index) => (
                                 <tr key={index}>
-                                    <td>{index + 1 + (currentPage - 1) * entriesPerPage}</td>
+                                    <td>{entriesPerPage === "All"
+                        ? index + 1 
+                        : index + 1 + (currentPage - 1) * entriesPerPage}</td>
                                     <td>{item.tup_id}</td>
                                     <td>{item.patron_fname}</td>
                                     <td>{item.patron_lname}</td>
@@ -191,18 +209,48 @@ const Logbook = () => {
                                     <td>{item.att_log_in_time}</td>
                                 </tr>
                             ))
-                        ) : (
+                        ) : patron.length==0 && !loading?(
                             <tr>
                                 <td colSpan="10">No records available</td>
                             </tr>
-                        )}
+                        ):(
+                            <tr>
+                            <td colSpan="7" style={{ textAlign: 'center', padding: '20px' }}>
+                              <div className="spinner-box">
+                                <div className="spinner-grow text-danger" role="status">
+                                  <span className="sr-only">Loading...</span>
+                                </div>
+                              </div>
+                            </td>
+                          </tr>
+                        ):''}
                     </tbody>
                     
                 </table>
-            </div>
+          
 
             {/* pagination */}
             <div className="logbook-table-pagination">
+                <p className="logbook-table-entries m-0">
+                    Showing {patron?patron.length:0} of {totalEntries}   Entries
+                </p>
+                {entriesPerPage !== "All" && (
+                    <div className="logbook-table-button-pagination">
+                        <button onClick={() => handlePageChange(currentPage - 1)} className="btn ">
+                            <FontAwesomeIcon icon={faArrowLeft} className='icon'/>
+                        </button>
+                        <div className="logbook-pages">
+                            Page {currentPage} of {totalPages}
+                        </div>
+                        <button onClick={() => handlePageChange(currentPage + 1)} className="btn ">
+                            <FontAwesomeIcon icon={faArrowRight} className='icon'/>
+                        </button>
+                    </div>
+                )}
+            </div>
+
+
+            {/* <div className="logbook-table-pagination">
                 <div className="logbook-table-entries">
                     Showing {patron.length} of {totalEntries} Entries
                 </div>
@@ -217,7 +265,7 @@ const Logbook = () => {
                         <img src={right} alt="" />
                     </button>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
