80cc3c71b981cf673bb826e356c9b9f20f9b1d98
lancewrt <91044173+lancewrt@users.noreply.github.com>
2025-03-13 12:48:03 +0800

    bb

--- admin/src/components/CatalogManage/CatalogManage.jsx
+++ admin/src/components/CatalogManage/CatalogManage.jsx
@@ -352,7 +352,7 @@ const CatalogManage = () => {
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary" onClick={handleSaveTopic}>Save changes</button>
+              <button type="button" class="btn btn-primary" style={{ backgroundColor: "#94152b", borderColor: "#94152b" }} onClick={handleSaveTopic}>Save changes</button>
             </div>
           </div>
         </div>
