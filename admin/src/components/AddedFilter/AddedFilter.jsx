1f86c29307a478d0a572b8cbd6e1aa1cdc21e7e9
Pauleen Dingcong <pauleenanndingcong@gmail.com>
2025-03-28 11:18:31 +0800

    fix filter deletion sa admin side

--- admin/src/components/AddedFilter/AddedFilter.jsx
+++ admin/src/components/AddedFilter/AddedFilter.jsx
@@ -3,7 +3,7 @@ import './AddedFilter.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const AddedFilter = ({index, filters, filterCondition, handleAddFilterChange, handleRemoveFilter}) => {
+const AddedFilter = ({index, addedFilter, filters, filterCondition, handleAddFilterChange, handleRemoveFilter}) => {
     console.log(index)
     const [filter, setFilter] = useState({
         logic:'and',
@@ -33,22 +33,22 @@ const AddedFilter = ({index, filters, filterCondition, handleAddFilterChange, ha
   return (
     <div className='mt-2 add-filter-box'>
         <div className='d-flex gap-2'>
-            <select name="logic" id="" className='form-select w-25' onChange={handleChange}>
+            <select name="logic" id="" className='form-select w-25' onChange={handleChange} value={addedFilter.logic}>
                 {logical.map(item=>(
                     <option value={item}>{item}</option>
                 ))}
             </select>
-            <select name="filter" id="" className='form-select w-50' onChange={handleChange}>
+            <select name="filter" id="" className='form-select w-50' onChange={handleChange} value={addedFilter.filter}>
                 {filters.map(item=>(
                     <option value={item}>{item}</option>
                 ))}
             </select>
-            <select name="condition" id="" className='form-select w-50' onChange={handleChange}>
+            <select name="condition" id="" className='form-select w-50' onChange={handleChange} value={addedFilter.condition}>
                 {filterCondition.map(item=>(
                     <option value={item}>{item}</option>
                 ))}
             </select>
-            <input type="text" name='input' className='w-100 ps-2 text-input' placeholder='Enter search value...' onChange={handleChange}/>
+            <input type="text" name='input' className='w-100 ps-2 text-input' placeholder='Enter search value...' onChange={handleChange} value={addedFilter.input}/>
             <button className="btn bg-danger text-white" onClick={handleRemoveFilter}>
                 <FontAwesomeIcon icon={faTrash}/>
             </button>
