c41b942e8e06c9e9630d63c80b17f0c186eb9533
lancewrt <91044173+lancewrt@users.noreply.github.com>
2025-04-02 16:20:43 +0800

    Update EditPatron.jsx

--- admin/src/components/EditPatron/EditPatron.jsx
+++ admin/src/components/EditPatron/EditPatron.jsx
@@ -13,7 +13,7 @@ const EditPatron = () => {
         patron_mobile: '',
         patron_email: '',
         category: 'Student',
-        patron_status:'',
+        patron_status:'Select Status',
         college: '',
         program: null,
         tup_id: 'TUPM-',
@@ -657,6 +657,7 @@ const EditPatron = () => {
                                             value={patronData.patron_status}
                                             onChange={handleChange}
                                         >
+                                            <option selected disabled>Select Status</option>
                                             <option value="active">Active</option>
                                             <option value="inactive">Inactive</option>
                                         </select>
