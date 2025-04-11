39a05bf52df22b604b0b93e79fdcf4eb6d9a9927
Lance <lancewrt@gmail.com>
2025-04-11 20:54:30 +0800

    Revert "change to websocket"
    This reverts commit 0a29b0af5b72a5743b416cc7b4a0692a5bbacc85.

--- server/routes/circulationRoutes.js
+++ server/routes/circulationRoutes.js
@@ -1,15 +1,12 @@
 import express from "express";
 import { checkIn, checkinSearch, checkOut, checkoutRecord, checkoutSearch } from "../controller/circulationController.js";
 
-export const circulationRoutesWss = (wss)=>{
-    const router = express.Router();
+const router = express.Router();
 
-    router.get('/checkout/search', checkoutSearch);
-    router.get('/checkin/search', checkinSearch);
-    router.get('/checkout-record', checkoutRecord)
-    router.post('/checkin',  (req, res) => checkIn(req, res, wss));
-    router.post('/checkout', (req, res) => checkOut(req, res, wss));  
+router.get('/checkout/search', checkoutSearch);
+router.get('/checkin/search', checkinSearch);
+router.get('/checkout-record', checkoutRecord)
+router.post('/checkin', checkIn);
+router.post('/checkout',checkOut);  
 
-    return router; 
-}
-
+export default router;
