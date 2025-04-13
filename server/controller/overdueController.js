2327d873b67210058eeadcbdd087c4bf3c370a8e
Pauleen Dingcong <pauleenanndingcong@gmail.com>
2025-04-14 01:00:35 +0800

    edit io

--- server/controller/overdueController.js
+++ server/controller/overdueController.js
@@ -232,7 +232,7 @@ const sendEmail = (email, name, tupid, borrowDate, borrowDue, resourceTitle, res
   });
 };
 
-export const checkOverdue = async () => {
+export const checkOverdue = async (io) => {
   console.log('checking overdue');
   const q = `
   SELECT 
@@ -316,7 +316,7 @@ export const checkOverdue = async () => {
                 }
 
                 // Use the io instance from the request object
-                req.io.emit('overdueUpdated');
+                io.emit('overdueUpdated');
 
                 console.log('New overdue entry created for checkout_id:', item.checkout_id);
                 // Send email to patron
