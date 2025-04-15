e1d22963a09bee40b02e11974f2b76d7470f1105
Pauleen Dingcong <pauleenanndingcong@gmail.com>
2025-04-15 23:46:37 +0800

    request new activation link

--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -1,15 +1,16 @@
 import express from 'express'
-import { activate, activateAccount, checkEmailIfExist, checkIfUnameExist, createAccount, deactivateAccount, deleteInvite, editAccount, getAccounts, invite, verifyToken, viewAccount } from '../controller/accountController.js'
+import { activate, activateAccount, checkEmailIfExist, checkIfUnameExist, createAccount, deactivateAccount, deleteInvite, editAccount, getAccounts, invite, newLink, verifyToken, viewAccount } from '../controller/accountController.js'
 
 const router = express.Router()
 
 router.get('/', getAccounts)
-router.post('/', createAccount)
-router.post('/invite', invite)
-router.post('/activate',activate)
 router.get('/verify-token', verifyToken)
 router.get('/check-email',checkEmailIfExist)
 router.get('/check-uname',checkIfUnameExist)
+router.post('/', createAccount)
+router.post('/invite', invite)
+router.post('/activate',activate)
+router.post('/request-new-link', newLink)
 router.delete('/delete-invite', deleteInvite)
 router.get('/:id', viewAccount)
 router.put('/:id', editAccount)
