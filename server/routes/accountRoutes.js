81b52a9d5c7417b3aa48f0579c0575f24a746938
Pauleen Dingcong <pauleenanndingcong@gmail.com>
2025-04-15 23:21:04 +0800

    form validation for creating accounts

--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { activate, activateAccount, checkEmailIfExist, createAccount, deactivateAccount, editAccount, getAccounts, invite, verifyToken, viewAccount } from '../controller/accountController.js'
+import { activate, activateAccount, checkEmailIfExist, checkIfUnameExist, createAccount, deactivateAccount, deleteInvite, editAccount, getAccounts, invite, verifyToken, viewAccount } from '../controller/accountController.js'
 
 const router = express.Router()
 
@@ -9,6 +9,8 @@ router.post('/invite', invite)
 router.post('/activate',activate)
 router.get('/verify-token', verifyToken)
 router.get('/check-email',checkEmailIfExist)
+router.get('/check-uname',checkIfUnameExist)
+router.delete('/delete-invite', deleteInvite)
 router.get('/:id', viewAccount)
 router.put('/:id', editAccount)
 router.put('/activate/:id', activateAccount)
