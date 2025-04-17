146309eb53b393f4fcc7ddefb8f8ce38cc2a65bf
Pauleen Dingcong <pauleenanndingcong@gmail.com>
2025-04-17 14:32:17 +0800

    update password

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { checkEmail, checkIsEmailVerified, checkSession, checkUsername, login, logout, profile, updateAccount, verifyEmail, verifyToken } from '../controller/userController.js';
+import { changePassword, checkEmail, checkIsEmailVerified, checkSession, checkUsername, login, logout, profile, updateAccount, verifyEmail, verifyPassword, verifyToken } from '../controller/userController.js';
 
 const router = express.Router();
 
@@ -8,7 +8,9 @@ router.post('/logout',logout);
 router.get('/check-session', checkSession);
 router.post('/verify-email', verifyEmail)
 router.get('/verify-token', verifyToken)
-router.get('/check-verified', checkIsEmailVerified)
+router.get('/check-verified', checkIsEmailVerified);
+router.get('/verify-password', verifyPassword);
+router.put('/change-password/:userId', changePassword)
 router.put('/update/:id', updateAccount)
 router.get('/profile/:id', profile)
 router.get('/check-username/:username', checkUsername)
